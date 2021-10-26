ec2692de919a135a3d91f3bdb38a7043c3faa962
GUO CHIH-LING <s3a832098@student.ncut.edu.tw>
2021-10-26 12:56:16 +0800

    設定三個Route

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\AboutController;
+    use App\Http\Controllers\HomeController;
+    use App\Http\Controllers\NewsController;
+    use App\Http\Controllers\AboutController;
 
-    Route::get('/', function () {
-        return view('home.blade.php');
-    });
 
-    Route::get('/about',function(){
-        return view('about.blade.php');
-    });
+    Route::get('/about',[AboutController::class,'index'])-> name('about.index');
+    Route::get('/',[HomeController::class,'index'])-> name('home.index');
+    Route::get('/news',[NewsController::class,'index'])-> name('news.index');
 
-    Route::get('/news',function(){
-        return view('news.blade.php');
-    });
-
-    Route::get('/about',[AboutController::class,'index'])-> name('home.index');
 ?>
 
